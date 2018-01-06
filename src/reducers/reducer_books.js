e920fb18b0f08c3e2a321ee8c62a9aac8ed27223
Artas47 <artasss47@gmail.com>
2018-01-06 18:31:45 +0100

    displaying list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return [
+        {title : 'javascript'},
+        {title : 'f;dskljfds'},
+        {title : 'wertbqwebr'},
+        {title : 'ttnw'},
+        {title : 'xbrqwerq'},
+        {title : 'tbqwerbqw'}
+    ]
+}
