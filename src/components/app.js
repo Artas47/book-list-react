b86431e76b10214e65c8b785386766fdb3b08ee4
Artas47 <artasss47@gmail.com>
2018-01-18 22:39:42 +0100

    actions

--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,11 @@ import BookList from '../containers/book-list';
 
 
 export default class App extends Component{
-    render(){
-        return ( <div>
-            <BookList />
-            </div>
+  render(){
+    return ( 
+      <div>
+        <BookList />
+      </div>
         )
     }
 }
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component{
   renderList(){
@@ -16,14 +18,23 @@ class BookList extends Component{
           this.renderList()
         }
       </ul>
-    )
+      )
+    }
   }
-}
 
 function mapStateToProps(state){
+  //Whatever is returend will show up as props inside of BookList
   return {
     books: state.books
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+
+//anythting returned from this function will end up as props on the booklist container
+function mapDispatchToProps(dispatch){
+  //whenever selectbook is called , the result should be passed to all of our reducers
+  return bindActionCreators({selectBook : selectBook}, dispatch);
+}
+
+// promote booklist from a component ta a conetiner - it need to know about this new dispatch method, selectbook. make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
